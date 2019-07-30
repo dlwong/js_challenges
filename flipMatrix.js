cf6556111675508f8f1fda49c85f7a06fe8c7e0c
Dorris Wong <pingki@sbcglobal.net>
2019-07-29 23:13:25 -0700

    flip matrix toy problem

--- flipMatrix.js
+++ flipMatrix.js
@@ -1,10 +1,21 @@
 const flipMatrix = (matrix) => {
- return matrix.reverse();
+ matrix = matrix.reverse();
 
+ for (let i = 0; i<matrix.length; i++){
+   for (let j = 0; j<matrix[i].length; j++){
+    if (i !== j){
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i]; 
+      matrix[j][i] = temp; 
+    }
+   }
+ }
+ return matrix;
+ 
 }
 
-let m = [ [7,8,9],
-          [2,3,4],
-          [1,5,0]];
+let m = [[7,8,9],
+         [2,3,4],
+         [1,5,0]];
 
 console.log(flipMatrix(m));
