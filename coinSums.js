c63e524ba75f214104d6f44138b9dfcb90fb2797
Dorris Wong <pingki@sbcglobal.net>
2019-09-27 18:56:58 -0600

    Start numIsland problem

---
+++ coinSums.js
@@ -0,0 +1,5 @@
+const coinSums = (coins, amount) =>{
+
+}
+
+console.log(coinSums([1,2,3],4));
---
+++ numIslands.js
@@ -0,0 +1,10 @@
+var numIslands = function(grid) {
+    
+};
+
+console.log(numIslands([
+  [1,1,0,0,0],
+  [1,1,0,0,0],
+  [0,0,1,0,0],
+  [0,0,0,1,1]
+]));
