aacb5d24903afea27b6ac0836234687f8fef1083
Dorris Wong <pingki@sbcglobal.net>
2019-09-30 01:52:04 -0700

    Start phoneBook problem

---
+++ phoneBook.js
@@ -0,0 +1,17 @@
+const phoneBook = (book, name) => {
+
+
+
+
+
+}
+
+
+const book = [
+  {name: 'alice', phone: '650-340-3403'}
+  {name: 'kane', phone: '650-230-2342'}
+  {name: 'adam', phone: '650-310-3256'}
+  {name: 'edith', phone: '650-342-3404'}
+]
+
+console.log(phoneBook(book,'kane'));
