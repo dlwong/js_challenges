733aea38b736237b915b2545b6c21a7e80328fd9
Dorris Wong <pingki@sbcglobal.net>
2019-09-30 02:03:26 -0700

    Solve brute force phoneBook

--- phoneBook.js
+++ phoneBook.js
@@ -1,5 +1,9 @@
-const phoneBook = (book, name) => {
-
+function phoneBook(book, name) {
+  for (let i = 0; i < book.length; i++){
+    if (book[i].name === name) {
+      return book[i].phone;
+    }
+  }
 
 
 
@@ -8,9 +12,9 @@ const phoneBook = (book, name) => {
 
 
 const book = [
-  {name: 'alice', phone: '650-340-3403'}
-  {name: 'kane', phone: '650-230-2342'}
-  {name: 'adam', phone: '650-310-3256'}
+  {name: 'alice', phone: '650-340-3403'},
+  {name: 'kane', phone: '650-230-2342'},
+  {name: 'adam', phone: '650-310-3256'},
   {name: 'edith', phone: '650-342-3404'}
 ]
 
