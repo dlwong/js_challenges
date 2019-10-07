90302f50fcd6bcd4d942310679279b0fd3906ffa
Dorris Wong <pingki@sbcglobal.net>
2019-10-07 15:51:58 -0700

    Solve twoSum

--- twoSum.js
+++ twoSum.js
@@ -9,3 +9,15 @@ var twoSum = function(nums, target) {
 //console.log(twoSum([2, 7, 11, 15], 9)) //[0,1]
 console.log(twoSum([3, 2, 4], 6)); //[1,2]
 //console.log(twoSum([-3,4,3,9],0)); //[0,2]
+
+var twoSum = function(nums, target) {
+  let mapped = new Map();
+
+  for (let i = 0; i < nums.length; i++){
+    if (mapped.has(target - nums[i])) {
+      return [mapped.get(target-nums[i]), i];
+    }else {
+      mapped.set(nums[i], i)
+    }
+  }
+};
