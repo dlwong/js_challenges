4cb41d802f0ed11fc3094d604a39042e8a3dff17
Dorris Wong <dorrisfordorris@gmail.com>
2019-10-15 23:22:45 -0700

    maxDepth.js

---
+++ maxDepth.js
@@ -0,0 +1,20 @@
+
+var maxDepth = function(root) {
+    
+    if (!root) return 0;
+    
+    const queue = [root];
+    
+    let depth = 0;
+    
+    while (queue.length) {
+        depth++;
+        const len = queue.length;
+        for (let i = 0; i < len; i++) {
+            if (queue[i].left) queue.push(queue[i].left);
+            if (queue[i].right) queue.push(queue[i].right);
+        }
+        queue.splice(0, len);
+    }
+    return depth;
+};
